4c8c88723d5c71bdc75c643c57e9795d6eca7833
Beyond-Logic <edemaero@gmail.com>
2021-04-06 00:42:00 +0100

    #new update on ReadConfigFileToHashMap

--- src/Beyond/Logic/ReadConfigFileToHashMap.java
+++ src/Beyond/Logic/ReadConfigFileToHashMap.java
@@ -1,149 +1,126 @@
 package Beyond.Logic;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Scanner;
 
 
 public class ReadConfigFileToHashMap {
-    public static void main(String[] args) {
-        String first = "src/Beyond/Logic/config.txt";
-        String second = "src/Beyond/Logic/config.txt.dev";
-        String third = "src/Beyond/Logic/config.txt.staging";
+    public static void main(String[] args) throws IOException {
+//        HashMap<String, String> config = new HashMap<String, String>();
 
-        // read
-        // config.txt
-        // config.txt.dev
-        // config.txt.staging
-        // files to HashMap
+        Scanner fileName = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Please Enter Environment");
+        String environment = fileName.nextLine();  // Read user input
 
-        Map<String, String> mapFromFirst = getHashMapFromConfig(first);
-        Map<String, String> mapFromSecond = getHashMapFromConfigDev(second);
-        Map<String, String> mapFromThird = getHashMapFromConfigStaging(third);
+        if(environment.equalsIgnoreCase("production")) {
+        try(FileReader reader = new FileReader("src/Beyond/Logic/config.txt")) {
+            Properties properties = new Properties();
+            properties.load(reader);
+            //
+            String dbname = properties.getProperty("dbname");
+            String host = properties.getProperty("host");
+            String applicationName = properties.getProperty("name");
+            String port = properties.getProperty("port");
+            String contextUrl = properties.getProperty("context-url");
+            String mode = properties.getProperty("mode");
+            String theme = properties.getProperty("theme");
+            String pipeline = properties.getProperty("pipeline");
+            String applicationName1 = properties.getProperty("name");
 
-        // iterate over HashMap entries
-        for(Map.Entry<String, String> entry : mapFromFirst.entrySet()){
-            System.out.println(entry.getKey() + " => " + entry.getValue());
-        }
-        for(Map.Entry<String, String> entry : mapFromSecond.entrySet()){
-            System.out.println(entry.getKey() + " => " + entry.getValue());
-        }
-        for(Map.Entry<String, String> entry : mapFromThird.entrySet()){
-            System.out.println(entry.getKey() + " => " + entry.getValue());
-        }
-    }
-    public static Map<String, String> getHashMapFromConfig(String filePath){
-        Map<String, String> mapFileContents = new LinkedHashMap<String, String>();
-        BufferedReader br = null;
+            System.out.println(dbname);
+            System.out.println(host);
+            System.out.println(applicationName);
+            System.out.println(port);
+            System.out.println(contextUrl);
+            System.out.println(mode);
+            System.out.println(theme);
+            System.out.println(pipeline);
+            System.out.println(applicationName1);
 
-        try {
-            //create file object
-            File config = new File(filePath);
 
-            //create Buffered Reader object from File
-            br = new BufferedReader( new FileReader(config) );
-            String line;
 
-            //read config line by line
-            while ( (line = br.readLine()) != null) {
-                //split the line by:
-                String[] section = line.split("=");
-                //first section is key, second is value
-                String key = section[0].trim();
-                String value = section[1].trim();
-
-                //put key, value in Hashmap if they are not empty
-                if(!key.equals("") && !value.equals(""))
-                    mapFileContents.put(key, value);
-            }
-        } catch (Exception e){
+            //
+        }     catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            //Always close the BufferedReader
-            if(br != null){
-                try {
-                    br.close();
-                } catch (Exception e){ };
+        }
+        }
+
+        else if (environment.equalsIgnoreCase("development")) {
+            try (FileReader reader = new FileReader("src/Beyond/Logic/config.txt.dev")) {
+                Properties properties = new Properties();
+                properties.load(reader);
+
+
+                //
+                String dbname = properties.getProperty("dbname");
+                String host = properties.getProperty("host");
+                String applicationName = properties.getProperty("name");
+                String port = properties.getProperty("port");
+                String contextUrl = properties.getProperty("context-url");
+                String mode = properties.getProperty("mode");
+                String theme = properties.getProperty("theme");
+                String pipeline = properties.getProperty("pipeline");
+                String applicationName1 = properties.getProperty("name");
+
+                System.out.println(dbname);
+                System.out.println(host);
+                System.out.println(applicationName);
+                System.out.println(port);
+                System.out.println(contextUrl);
+                System.out.println(mode);
+                System.out.println(theme);
+                System.out.println(pipeline);
+                System.out.println(applicationName1);
+
+                //
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+                else if (environment.equalsIgnoreCase("staging")){
+            try (FileReader reader = new FileReader("src/Beyond/Logic/config.txt.staging")) {
+                Properties properties = new Properties();
+                properties.load(reader);
+                //
+                String dbname = properties.getProperty("dbname");
+                String host = properties.getProperty("host");
+                String applicationName = properties.getProperty("name");
+                String port = properties.getProperty("port");
+                String contextUrl = properties.getProperty("context-url");
+                String mode = properties.getProperty("mode");
+                String theme = properties.getProperty("theme");
+                String pipeline = properties.getProperty("pipeline");
+                String applicationName1 = properties.getProperty("name");
+
+                System.out.println(dbname);
+                System.out.println(host);
+                System.out.println(applicationName);
+                System.out.println(port);
+                System.out.println(contextUrl);
+                System.out.println(mode);
+                System.out.println(theme);
+                System.out.println(pipeline);
+                System.out.println(applicationName1);
+                //
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-        } return mapFileContents;
+        } else {
+            System.out.println("Environment not found, Try again");
+        }
 
-    }
 
-    public static Map<String, String> getHashMapFromConfigDev(String filePath){
-        Map<String, String> mapFileContents = new LinkedHashMap<String, String>();
-        BufferedReader br = null;
 
-        try {
-            //create file object
-            File configdev = new File(filePath);
 
-            //create Buffered Reader object from File
-            br = new BufferedReader( new FileReader(configdev) );
-            String line;
 
-            //read configdev line by line
-            while ( (line = br.readLine()) != null) {
-                //split the line by:
-                String[] section = line.split("=");
-                //first section is key, second is value
-                String key = section[0].trim();
-                String value = section[1].trim();
+        }
 
-                //put key, value in Hashmap if they are not empty
-                if(!key.equals("") && !value.equals(""))
-                    mapFileContents.put(key, value);
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        } finally {
-            //Always close the BufferedReader
-            if(br != null){
-                try {
-                    br.close();
-                } catch (Exception e){ };
-            }
+        }
 
-        } return mapFileContents;
 
-    }
-
-    public static Map<String, String> getHashMapFromConfigStaging(String filePath){
-        Map<String, String> mapFileContents = new LinkedHashMap<String, String>();
-        BufferedReader br = null;
-
-        try {
-            //create file object
-            File configstaging = new File(filePath);
-
-            //create Buffered Reader object from File
-            br = new BufferedReader( new FileReader(configstaging) );
-            String line;
-
-            //read configstaging line by line
-            while ( (line = br.readLine()) != null) {
-                //split the line by:
-                String[] section = line.split("=");
-                //first section is key, second is value
-                String key = section[0].trim();
-                String value = section[1].trim();
-
-                //put key, value in Hashmap if they are not empty
-                if(!key.equals("") && !value.equals(""))
-                    mapFileContents.put(key, value);
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        } finally {
-            //Always close the BufferedReader
-            if(br != null){
-                try {
-                    br.close();
-                } catch (Exception e){ };
-            }
-
-        } return mapFileContents;
-
-    }
-}
