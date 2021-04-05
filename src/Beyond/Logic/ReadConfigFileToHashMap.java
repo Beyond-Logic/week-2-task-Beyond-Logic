269b64c643054effca5873653548e7afa2f765e0
Beyond-Logic <edemaero@gmail.com>
2021-04-05 21:09:22 +0100

    #new update

--- src/Beyond/Logic/ReadConfigFileToHashMap.java
+++ src/Beyond/Logic/ReadConfigFileToHashMap.java
@@ -7,16 +7,33 @@ import java.util.Map;
 
 
 public class ReadConfigFileToHashMap {
-    final static String filePath = "/Users/decagon/Desktop/Java/week-2-task-Beyond-Logic/src/Beyond/Logic/config.txt";
     public static void main(String[] args) {
-        // read text file to HashMap
-        Map<String, String> mapFromFile = getHashMapFromTextFile();
+        String first = "src/Beyond/Logic/config.txt";
+        String second = "src/Beyond/Logic/config.txt.dev";
+        String third = "src/Beyond/Logic/config.txt.staging";
+
+        // read
+        // config.txt
+        // config.txt.dev
+        // config.txt.staging
+        // files to HashMap
+
+        Map<String, String> mapFromFirst = getHashMapFromConfig(first);
+        Map<String, String> mapFromSecond = getHashMapFromConfigDev(second);
+        Map<String, String> mapFromThird = getHashMapFromConfigStaging(third);
+
         // iterate over HashMap entries
-        for(Map.Entry<String, String> entry : mapFromFile.entrySet()){
-            System.out.println(entry.getKey());
+        for(Map.Entry<String, String> entry : mapFromFirst.entrySet()){
+            System.out.println(entry.getKey() + " => " + entry.getValue());
+        }
+        for(Map.Entry<String, String> entry : mapFromSecond.entrySet()){
+            System.out.println(entry.getKey() + " => " + entry.getValue());
+        }
+        for(Map.Entry<String, String> entry : mapFromThird.entrySet()){
+            System.out.println(entry.getKey() + " => " + entry.getValue());
         }
     }
-    public static Map<String, String> getHashMapFromTextFile(){
+    public static Map<String, String> getHashMapFromConfig(String filePath){
         Map<String, String> mapFileContents = new LinkedHashMap<String, String>();
         BufferedReader br = null;
 
@@ -26,19 +43,95 @@ public class ReadConfigFileToHashMap {
 
             //create Buffered Reader object from File
             br = new BufferedReader( new FileReader(config) );
-            String line = null;
+            String line;
 
             //read config line by line
             while ( (line = br.readLine()) != null) {
                 //split the line by:
-                String[] parts = line.split(":");
-                //first part is key, second is value
-                String key = parts[0].trim();
-                String value = parts[0].trim();
+                String[] section = line.split("=");
+                //first section is key, second is value
+                String key = section[0].trim();
+                String value = section[1].trim();
 
                 //put key, value in Hashmap if they are not empty
                 if(!key.equals("") && !value.equals(""))
-                    mapFileContents.put(key,value);
+                    mapFileContents.put(key, value);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            //Always close the BufferedReader
+            if(br != null){
+                try {
+                    br.close();
+                } catch (Exception e){ };
+            }
+
+        } return mapFileContents;
+
+    }
+
+    public static Map<String, String> getHashMapFromConfigDev(String filePath){
+        Map<String, String> mapFileContents = new LinkedHashMap<String, String>();
+        BufferedReader br = null;
+
+        try {
+            //create file object
+            File configdev = new File(filePath);
+
+            //create Buffered Reader object from File
+            br = new BufferedReader( new FileReader(configdev) );
+            String line;
+
+            //read configdev line by line
+            while ( (line = br.readLine()) != null) {
+                //split the line by:
+                String[] section = line.split("=");
+                //first section is key, second is value
+                String key = section[0].trim();
+                String value = section[1].trim();
+
+                //put key, value in Hashmap if they are not empty
+                if(!key.equals("") && !value.equals(""))
+                    mapFileContents.put(key, value);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            //Always close the BufferedReader
+            if(br != null){
+                try {
+                    br.close();
+                } catch (Exception e){ };
+            }
+
+        } return mapFileContents;
+
+    }
+
+    public static Map<String, String> getHashMapFromConfigStaging(String filePath){
+        Map<String, String> mapFileContents = new LinkedHashMap<String, String>();
+        BufferedReader br = null;
+
+        try {
+            //create file object
+            File configstaging = new File(filePath);
+
+            //create Buffered Reader object from File
+            br = new BufferedReader( new FileReader(configstaging) );
+            String line;
+
+            //read configstaging line by line
+            while ( (line = br.readLine()) != null) {
+                //split the line by:
+                String[] section = line.split("=");
+                //first section is key, second is value
+                String key = section[0].trim();
+                String value = section[1].trim();
+
+                //put key, value in Hashmap if they are not empty
+                if(!key.equals("") && !value.equals(""))
+                    mapFileContents.put(key, value);
             }
         } catch (Exception e){
             e.printStackTrace();
